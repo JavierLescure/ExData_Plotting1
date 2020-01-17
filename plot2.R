8a56016487fd836c5f0ed907bda53b6ab5c57c6a
JavierLescure <51793696+JavierLescure@users.noreply.github.com>
2020-01-17 20:04:04 +0100

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(dplyr)
+
+# Load the dataset
+dataset <- read.delim("household_power_consumption.txt", sep = ";", dec = ".")
+summary(dataset)
+
+# Prepare the dataset
+dataset$Date <- as.Date(dataset$Date, format = "%d/%m/%Y")
+dataPrep <- filter(dataset, 
+                   dataset$Date == "2007-02-01" | dataset$Date == "2007-02-02")
+dataPrep$Global_active_power <- 
+    as.numeric(levels(dataPrep$Global_active_power))[dataPrep$Global_active_power]
+dataPrep$Global_reactive_power <- 
+    as.numeric(levels(dataPrep$Global_reactive_power))[dataPrep$Global_reactive_power]
+dataPrep$Voltage <- 
+    as.numeric(levels(dataPrep$Voltage))[dataPrep$Voltage]
+dataPrep$Global_intensity <- 
+    as.numeric(levels(dataPrep$Global_intensity))[dataPrep$Global_intensity]
+dataPrep$Sub_metering_1 <- 
+    as.numeric(levels(dataPrep$Sub_metering_1))[dataPrep$Sub_metering_1]
+dataPrep$Sub_metering_2 <- 
+    as.numeric(levels(dataPrep$Sub_metering_2))[dataPrep$Sub_metering_2]
+dataPrep$Sub_metering_3 <- 
+    as.numeric(levels(dataPrep$Sub_metering_3))[dataPrep$Sub_metering_3]
+dataPrep$Time2 <-paste(dataPrep$Date," ",dataPrep$Time)
+dataPrep$Time2 <- as.POSIXlt(dataPrep$Time2,format="%Y-%m-%d %H:%M:%S")
+
+# Construct the plot
+plot(dataPrep$Time2 , dataPrep$Global_active_power,  type="l",
+     ylab="Global Active Power (kilowatts)", xlab = "")
+
+# Save the plot in a PNG file
+dev.copy(png, file = "plot2.png", width = 480, height = 480, unit = "px")
+dev.off()
+
+
+
+
+
+
+
